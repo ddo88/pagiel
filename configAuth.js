ab8d3f54c7c7d9a8d550ba24c59dabf6384bf4e3
Daniel Durán <ddo88@hotmail.com>
2017-06-10 11:23:13 -0500

    config

--- configAuth.js
+++ configAuth.js
@@ -1,8 +1,7 @@
 module.exports = {
     'googleAuth' : {
-        'clientID'      : '550272492866-0ri7qnq88os28c6nu3t6k82qnb0ft75r.apps.googleusercontent.com',
-        'clientSecret'  : 'C8iYLtdiuNzEHEWkbvWJEuK3',
-        'callbackURL'   : 'https://pagiel.herokuapp.com/oauth2callback'
-        //'callbackURL'   : 'http://localhost:3000/oauth2callback'
+        'clientID'      : process.env.ClientID,
+        'clientSecret'  : process.env.clientSecret,
+        'callbackURL'   : process.env.CallbackURL || 'http://localhost:3000/oauth2callback'
     }
 };
