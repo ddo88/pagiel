a06f05b57901c305b7421069d656d277b211ad7c
Daniel Durán <=daniel.duran@mamboo.com>
2023-10-20 22:09:15 -0500

    zip de upload package

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
-//var passport=require('passport');
-// var configAuth=require('./configAuth.js');
-// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 /* GET home page. */
 router.get('/',function(req,res,next){
-  //res.redirect('/presentation');
   res.render('index',{});
 })
 router.get('/songs', function(req, res, next) {
