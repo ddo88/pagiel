866fe58c02058702847b934a10961a983fe506e0
ddo88 <ddo88@hotmail.com>
2023-11-21 20:54:23 -0500

    Update list.js

--- public/javascripts/list.js
+++ public/javascripts/list.js
@@ -153,7 +153,7 @@ function VM()
             change: _self.buscar,
             tagMode: "single",
             height:250,
-            maxSelectedItems: 10
+            maxSelectedItems: 30
         });
       };
     
